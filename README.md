d378c545a6c125d3717fa2ea68de347560cf3c06
Rodell <rodellrodriguez53@gmail.com>
2017-10-22 19:04:52 -0400

    Updating Readme and source code

--- README.md
+++ README.md
@@ -2,9 +2,20 @@
 
 **Description**
 
-It is my first time playing Fantasy Basketball and my friends decided to use Yahoo's Fantasy Basketball website. The NBA has 82 games in the season so that means I would need to choose which of my players to start at least 82 times. I decided to create a Python script, utilizing the Selenium library, that automates the task of starting your active players for as many days as the user desires. Active means a player is playing on the corresponding date. Additionally, if an active player ends up on the bench, the user will get notified with the date and player that this situation applies to. This safety measure is so that the user can look into this matter and decide for himself/herself who to start and who to bench.
+It is my first time playing Fantasy Basketball and my friends decided to use Yahoo's Fantasy Basketball website. The NBA has 82 games in the season so that means I would need to choose which of my players to start at least 82 times. I decided to create a Python script, utilizing the Selenium library, that automates the task of starting your active players for as many days as the user desires. Active means a player's team is playing on the corresponding date. 
 
-The default number of days that the script will automate is set to 7 days. Feel free to change the number as you see fit.
+4 cases where the report will tell the user to investigate their roster:
+1. When an active player is on the bench
+2. When an active player's status is Injured
+3. When an active player's status is Out
+4. When an active player's status is GTD
+
+If any of these 4 situations occur, then it is in the user's best interest to manually set up their roster for that day.
+
+Note:
+1. The default number of days that the script will automate is set to 7 days. Feel free to change the number in the source code as the user sees fit.
+2. This script assumes the user is only enrolled in ONE Yahoo Fantasy League. I have not accounted for multiple leagues
+3. This script assumes the user has 13 players. I have not accounted for 14 players if the user has an injured player
 
 **Dependencies**
 * Python 3.5
@@ -13,15 +24,16 @@ The default number of days that the script will automate is set to 7 days. Feel 
 
 **Setup**
 1. Download script
-2. Change any of the capitilized variables in the script as necessary e.g. YAHOO_EMAIL, YAHOO_PASSWORD, and CHROME_DRIVER_PATH for the location of your chromedriver.exe file.
-3. Change NUM_OF_PLAYERS variable to 14 if you are allowed to have an extra player on your roster if one of your players is injured
+2. Change CHROME_DRIVER_PATH to the location of your chromedriver.exe file if necessary.
+3. Change NUM_OF_PLAYERS variable if necessary
 4. Install dependencies
 5. Run Program
 
 **To-Do List**
 
-- [ ] Exception handling
-- [x] Do a report for x number of days
-- [x] Have the report mention if any benched players are Active
+- [x] Capability of creating a report for x number of days
+- [x] Report status of a player e.g. Healthy, Injured, Out, Game Time Decision(GTD)
+- [x] Have the report mention if any Benched, Injured, Out, and GTD players are Active
+- [x] Have the script prompt the user for their yahoo email, password, and team name instead of editing the source code
 - [ ] Account for fantasy rule where you can have an additional player on your roster if one of your players is injured
-- [ ] Report status of a player e.g. Healthy, Injured, Out, Game Time Decision(GTD)
+- [ ] Exception handling
--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+#getpass is a secure way for people to input their password without it displaying on the terminal
+import getpass
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
@@ -22,21 +25,64 @@ def explicit_wait_by_xpath(driver, xpath):
 def print_active_bench_players(active_bench_list):
 	print('Players who are playing but are assigned to the bench:')
 	for x in range(0,len(active_bench_list)):
-		print(active_bench_list[x][0] + " " + active_bench_list[x][1])
+		print(active_bench_list[x][0] 
+			+ ", " + active_bench_list[x][1])
+
+#Each element of the list will be a list of 3 elements: Date, Player Name, and their Status
+def print_active_ineligible_status_players(active_ineligible_status_list):
+	print('Players who are playing but might not play:')
+	for x in range(0,len(active_ineligible_status_list)):
+		print(active_ineligible_status_list[x][0] 
+			+ ", " + active_ineligible_status_list[x][1] 
+			+ ", " + active_ineligible_status_list[x][2])	
 
 def start_active_players(driver):
 	print("Starting active players...")
 	driver.find_element_by_xpath("//*[contains(text(), 'Start Active Players')]").click()	
 
 def click_next_date(driver):
+	print("Clicking next date...")
 	driver.find_element_by_xpath("//a[@class='Js-next Grid-u No-bdr-radius-start No-bdrstart Pstart-med Td-n Fz-xs ']/span").click() 
+
+def get_email():
+	prompt = 'Please enter yahoo email. You do not need to input @yahoo.com:\n'
+	while True:
+		email = input(prompt)
+		prompt_confirmation = 'Re-enter your email for confirmation\n'
+		email_confirmation = input(prompt_confirmation)
+		if email == email_confirmation: 
+			return email
+		else:
+			print('Emails do not match. Try again.') 
+
+def get_password():
+	prompt = 'Please enter yahoo password:\n'
+	while True:
+		password = getpass.getpass(prompt)
+		prompt_confirmation = 'Re-enter your password for confirmation\n'
+		password_confirmation = getpass.getpass(prompt_confirmation)
+		if password == password_confirmation: 
+			return password
+		else:
+			print('Passwords do not match. Try again.')
+
+def get_team_name():
+	prompt = 'Please enter the exact name of your team:\n'
+	while True:
+		team_name = input(prompt)
+		prompt_confirmation = 'Re-enter your team name for confirmation\n'
+		team_name_confirmation = input(prompt_confirmation)
+		if team_name == team_name_confirmation: 
+			return team_name
+		else:
+			print('Team names do not match. Try again.')
 
 def main():
 #Required setup to access Yahoo Fantasy Basketball. Change capitilized variables as needed
 	URL = 'https://basketball.fantasysports.yahoo.com/'
-	YAHOO_EMAIL = 'xxxxxxx'
-	YAHOO_PASSWORD = 'xxxxxxx'
-	TEAM_NAME = 'The Clinic'
+	YAHOO_EMAIL = get_email()
+	YAHOO_PASSWORD = get_password()
+	TEAM_NAME = get_team_name()
 	NUM_OF_PLAYERS = 13
 	NUM_OF_DAYS = 7 
 
@@ -60,39 +106,57 @@ def main():
 
 #Getting Player Information
 	active_bench_list = []
+	active_ineligible_status_list = []
 	
 	for day in range(0,NUM_OF_DAYS):
+		print('*' * 50)
 		date = driver.find_element_by_xpath("//span[@id='selectlist_nav']/a[2]").text
-		print("Date is: " + date + "\n")
+		print("Date is: " + date)
 		start_active_players(driver)
 		
 		for x in range(1,NUM_OF_PLAYERS+1):
 		#tr[x] where x is the x'th player row. First element is 1 NOT 0
+			print()
 			player_name_team_base_xpath = "//table/tbody/tr[" + str(x)+ "]"
 			player_name_xpath = player_name_team_base_xpath + "/td[3]/div/div/div/a"
 			player_team_xpath = player_name_team_base_xpath + "/td[3]/div/div/div/span"
 			player_position_xpath = player_name_team_base_xpath +"/td[1]/div/span"
 			player_opponent_xpath = player_name_team_base_xpath + "/td[5]/div"
+			player_status_xpath = player_name_team_base_xpath + "/td[3]/div/div[2]/span"
+
 			player_name = driver.find_element_by_xpath(player_name_xpath).text
 			player_team = driver.find_element_by_xpath(player_team_xpath).text
 			player_position = driver.find_element_by_xpath(player_position_xpath).text
 			player_opponent = driver.find_element_by_xpath(player_opponent_xpath).text
+			player_status = driver.find_element_by_xpath(player_status_xpath).text
+		#If we receive an empty string
+			if not player_status: 
+				player_status = 'Healthy'
+		#Start printing player information
 			print('Player: ', player_name)
 			print('Team: ', player_team)
 			print('Fantasy Basketball Position: ', player_position)
-			if not player_opponent:
-				print(player_name + ' is not playing today')
-			else: # Player is active
+			if player_opponent:
 				print('Opponent: ', player_opponent)
-				if player_position == 'BN':
+			#Check the 4 Cases and notify user if any of them occur
+				if player_position == 'BN': 
 					active_bench_list.append([date, player_name])
-			print()
-		
-		if active_bench_list:
-			print()
-			print_active_bench_players(active_bench_list)
-
+				if player_status == 'Injured' or \
+					player_status == 'Out' or \
+					player_status == 'GTD': 
+						active_ineligible_status_list.append([date, player_name, player_status])
+			else: 
+				print(player_name + ' is not playing today')
+			print('Status: ', player_status)
+	
 		click_next_date(driver)
+#Print report	
+	if active_bench_list:
+		print()
+		print_active_bench_players(active_bench_list)
+	if active_ineligible_status_list:
+		print()
+		print_active_ineligible_status_players(active_ineligible_status_list)
 
 #Shutdown
 	input('Enter any key to exit')
