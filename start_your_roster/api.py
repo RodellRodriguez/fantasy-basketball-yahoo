d06f1f59cf391f371b5ced9d367e8f85aea5e322
Rodell <rodellrodriguez53@gmail.com>
2017-12-12 22:53:51 -0500

    Restructured repo for another Python program

---
+++ start_your_roster/api.py
@@ -0,0 +1,12 @@
+from requests_oauthlib import OAuth2Session
+
+client_id = 'dj0yJmk9d3pTR1dENHJvQUZBJmQ9WVdrOWIyeE9aMHBOTjJFbWNHbzlNQS0tJnM9Y29uc3VtZXJzZWNyZXQmeD1lMA--'
+client_secret = '31d5654e6304da239f1191ed227933f061d31a5e'
+redirect_uri = 'http://localhost:8000/graphql'
+
+yahoo_oauth_url = 'https://api.login.yahoo.com/oauth2/request_auth'
+yahoo_oauth_get_token_url = 'https://api.login.yahoo.com/oauth2/get_token'
+
+league_id = '160754'
+
+
